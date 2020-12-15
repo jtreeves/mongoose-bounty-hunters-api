1f8d5b301efe52bec82301267fe4bee54abf4a8b
jtreeves <jackson.t.reeves@gmail.com>
2020-12-15 12:42:42 -0500

    Add CRUD routes

--- controllers/bountiesControllers.js
+++ controllers/bountiesControllers.js
@@ -1,5 +1,23 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.send('bounties')
+    res.send('hello from GET /bounties')
 })
+
+router.get('/:id', (req, res) => {
+    res.send('hello from GET /bounties/:id')
+})
+
+router.post('/', (req, res) => {
+    res.send('hellow from POST /bounties')
+})
+
+router.put('/:id', (req, res) => {
+    res.send('hello from PUT /bounties/:id')
+})
+
+router.delete('/:id', (req, res) => {
+    res.send('hello from DELETE bounties/:id')
+})
+
+module.exports = router
